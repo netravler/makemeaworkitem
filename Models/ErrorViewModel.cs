9ad73a1dbafbd53d603b34a64520b4f26a1cfdec
Paul Heintz <pheintz@hotmail.com>
2021-06-21 19:43:20 -0500

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MakeMeAWorkItem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
